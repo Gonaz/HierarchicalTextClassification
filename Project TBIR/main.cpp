472a880fd655ece077e6bb3af865da9ef62f570f
Jonas Vanthornhout <Jonas@Jonas-MBA.local>
2013-02-23 11:16:32 +0100

    Initial Commit

---
+++ Project TBIR/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project TBIR
+//
+//  Created by Jonas Vanthornhout on 23/02/13.
+//  Copyright (c) 2013 Jonas Vanthornhout. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
